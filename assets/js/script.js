d3085b12568776fa5307cafbeedcffc095859acd
codri5 <97296166+codri5@users.noreply.github.com>
2023-02-02 21:13:41 +0000

    updated js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $('#search-btn').on('click', function() {
     let query = $('#InputSearchTerm').val();
-
+   
     queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + 
     query + '&api-key=WR5qqFxqdO9d754CMX1HbY13Em5fz58a'
 
@@ -10,15 +10,16 @@ $('#search-btn').on('click', function() {
     }).then(function(response) {
         console.log(response)
         let results = response.response.docs;
-        for (let i = 0; i < results.length; i++) {
+        let articleNum = parseInt($('#FormControlSelect').val());
+        for (let i = 0; i < articleNum; i++) {
             let articleDiv = $('<div>');
             articleDiv.addClass('articleDiv');
             let headline = $('<h4>');
             let abstract = $('<p>');
             headline = headline.text(results[i].headline.main);
             abstract = abstract.text(results[i].abstract);
-            articleDiv.prepend(abstract);
-            articleDiv.prepend(headline);
+            articleDiv.append(headline);
+            articleDiv.append(abstract);
             $('.articles').prepend(articleDiv);
         }
     })
@@ -26,4 +27,4 @@ $('#search-btn').on('click', function() {
 
 $('#clear-btn').on('click', function() {
     $('.articles').empty();
-})
+});

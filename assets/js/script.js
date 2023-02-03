e37d50d336f0808224102e8b6bbcfbe66266c08c
codri5 <97296166+codri5@users.noreply.github.com>
2023-02-03 12:13:45 +0000

    added a tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+let query = $('#InputSearchTerm').val();
+
 $('#search-btn').on('click', function() {
-    let query = $('#InputSearchTerm').val();
+
    
     queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + 
     query + '&api-key=WR5qqFxqdO9d754CMX1HbY13Em5fz58a'
@@ -8,19 +10,25 @@ $('#search-btn').on('click', function() {
         url: queryURL,
         method: 'GET'
     }).then(function(response) {
-        console.log(response)
         let results = response.response.docs;
         let articleNum = parseInt($('#FormControlSelect').val());
         for (let i = 0; i < articleNum; i++) {
             let articleDiv = $('<div>');
-            articleDiv.addClass('articleDiv');
-            let headline = $('<h4>');
+            let headline = $('<a>');
             let abstract = $('<p>');
+            articleDiv.addClass('articleDiv');    
             headline = headline.text(results[i].headline.main);
             abstract = abstract.text(results[i].abstract);
+            let articleUrl = results[i].web_url;
+            headline.attr('href', articleUrl);
+            headline.attr('target', '_blank');
             articleDiv.append(headline);
             articleDiv.append(abstract);
             $('.articles').prepend(articleDiv);
+            if (query != '') {
+                console.log('hello');
+            }
+      
         }
     })
 });

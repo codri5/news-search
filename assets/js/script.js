3d3eada432b304fc9ccda8145034b931e038b82d
codri5 <97296166+codri5@users.noreply.github.com>
2023-02-06 14:28:08 +0000

    updated articles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,49 @@
-let query = $('#InputSearchTerm').val();
+'use strict';
 
 $('#search-btn').on('click', function() {
 
-   
-    queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + 
-    query + '&api-key=WR5qqFxqdO9d754CMX1HbY13Em5fz58a'
-
+    let query = $('#InputSearchTerm').val();
+    let queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + 
+    query + '&api-key=WR5qqFxqdO9d754CMX1HbY13Em5fz58a';
+    
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function(response) {
+        $('.articles').empty();
+        console.log(response)
         let results = response.response.docs;
-        let articleNum = parseInt($('#FormControlSelect').val());
-        for (let i = 0; i < articleNum; i++) {
+        let recordNum = parseInt($('#FormControlSelect').val());
+        for (let i = 0; i < recordNum; i++) {
+            let articleNum = $('<p>');
+            articleNum.addClass('article-number');
             let articleDiv = $('<div>');
+            articleDiv.addClass('articleDiv');   
             let headline = $('<a>');
-            let abstract = $('<p>');
-            articleDiv.addClass('articleDiv');    
+            let author = $('<p>');
+            let section = $('<p>');
+            let pubDate = $('<p>');
+            let articleUrl = $('<a>')
+            articleUrl.addClass('url');
             headline = headline.text(results[i].headline.main);
-            abstract = abstract.text(results[i].abstract);
-            let articleUrl = results[i].web_url;
-            headline.attr('href', articleUrl);
-            headline.attr('target', '_blank');
-            articleDiv.append(headline);
-            articleDiv.append(abstract);
-            $('.articles').prepend(articleDiv);
-            if (query != '') {
-                console.log('hello');
-            }
-      
+            author = author.text(results[i].byline.original);
+            section = section.text(`Section: ${results[i].section_name}`);
+            pubDate = pubDate.text((results[i].pub_date).slice(0, 10));
+            articleUrl.attr('href', results[i].web_url);
+            articleUrl.attr('target', '_blank');
+            articleUrl = articleUrl.text(results[i].web_url);
+            articleDiv.append(articleNum, headline, author, section, pubDate, articleUrl);
+            $('.articles').append(articleDiv);
+
+            let year = parseInt(results[i].pub_date.slice(0, 4));
+            let startYear = parseInt($('#InputStartYear').val());
+            let endYEar = parseInt($('#InputEndYear').val());
         }
-    })
+        let artNum = 1;
+        $('.article-number').each(function() {
+            $(this).text(artNum++);
+        });
+    });
 });
 
 $('#clear-btn').on('click', function() {

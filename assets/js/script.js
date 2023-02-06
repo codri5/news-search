a6c8f740d3e33f262a6d3504c7f35a195ff0d2f6
codri5 <97296166+codri5@users.noreply.github.com>
2023-02-06 18:20:38 +0000

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,54 @@
-'use strict';
-
+// Event listener
 $('#search-btn').on('click', function() {
-
-    let query = $('#InputSearchTerm').val();
+    // Retrieve user input
+    let query = $('#InputSearchTerm').val().trim();
+    // URL used to query the API
     let queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + 
     query + '&api-key=WR5qqFxqdO9d754CMX1HbY13Em5fz58a';
-    
+    // AJAX request to API
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function(response) {
+        // Clear articles container 
         $('.articles').empty();
-        console.log(response)
         let results = response.response.docs;
+        // Retrieve number of articles
         let recordNum = parseInt($('#FormControlSelect').val());
         for (let i = 0; i < recordNum; i++) {
-            let articleNum = $('<p>');
-            articleNum.addClass('article-number');
+            // Create elements
             let articleDiv = $('<div>');
-            articleDiv.addClass('articleDiv');   
+            let articleNum = $('<p>');
             let headline = $('<a>');
             let author = $('<p>');
             let section = $('<p>');
             let pubDate = $('<p>');
-            let articleUrl = $('<a>')
+            let articleUrl = $('<a>');
+            // Add classes
+            articleNum.addClass('article-number');
+            articleDiv.addClass('articleDiv'); 
             articleUrl.addClass('url');
+            // Add attributes to article url
+            articleUrl.attr('href', results[i].web_url);
+            articleUrl.attr('target', '_blank');
+            // Add retrieved values to elements
             headline = headline.text(results[i].headline.main);
             author = author.text(results[i].byline.original);
             section = section.text(`Section: ${results[i].section_name}`);
             pubDate = pubDate.text((results[i].pub_date).slice(0, 10));
-            articleUrl.attr('href', results[i].web_url);
-            articleUrl.attr('target', '_blank');
             articleUrl = articleUrl.text(results[i].web_url);
+            // Append elements
             articleDiv.append(articleNum, headline, author, section, pubDate, articleUrl);
             $('.articles').append(articleDiv);
-
-            let year = parseInt(results[i].pub_date.slice(0, 4));
-            let startYear = parseInt($('#InputStartYear').val());
-            let endYEar = parseInt($('#InputEndYear').val());
         }
-        let artNum = 1;
-        $('.article-number').each(function() {
-            $(this).text(artNum++);
-        });
+            // Add article count
+            let count = 1;
+            $('.article-number').each(function() {
+                $(this).text(count++);
+            });
     });
 });
-
+// Event listener to clear articles
 $('#clear-btn').on('click', function() {
     $('.articles').empty();
 });
